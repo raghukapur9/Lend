2433a1a255576702fbcd1e643fe1f36de59d6923
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-03-14 23:12:55 +0530

    added stableCoin addition code

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -4,14 +4,15 @@ contract Migrations {
     address owner = msg.sender;
 
     address[] public stableCoins;
-    uint public stableCoinsIndex = 0;
+    uint stableCoinsIndex = 0;
 
     constructor() public {
         owner = msg.sender;
     }
     function addStableCoin (address _stableCoinAddress) public {
+        require((msg.sender == owner), "Stable coin address cannot be zero");
         require((_stableCoinAddress != address(0)), "Stable coin address cannot be zero");
-        stableCoins[stableCoinsIndex] = _stableCoinAddress;
+        stableCoins.push(_stableCoinAddress);
+        stableCoinsIndex ++;
     }
-
 }
